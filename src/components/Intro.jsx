e15fbdf48d007be85276cf6e4634303b297fae22
Anshika05-dev <anshika27.asr@gmail.com>
2025-03-31 16:12:18 +0530

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import profile from '../Images/profile.jpg'
+import profile from '../images/profile.jpg'
 import './Intro.css'
 import { Link } from 'react-scroll'
 import Navbar from './Navbar'
